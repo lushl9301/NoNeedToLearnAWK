9175d2e9b0829831b1fe641ed3c9df8bf51e5ddd
Lu Shengliang <lushl9301@gmail.com>
2014-09-20 10:12:36 +0800

    add example

---
+++ ureca/submit/README.md
@@ -0,0 +1,4 @@
+1. Use ```rowData.txt > pre-process.pl > myFormat```. *rowData.txt* is the row data downloaded from the database.
+    This process is use to generate the format I would like to use to do further analysis.
+2. Use ```myFormat.txt > select_sentence.pl > result.txt```. 
+    Analyse this text file. And select useful sentence.
---
+++ ureca/submit/pre-process.pl
@@ -0,0 +1,81 @@
+use warnings;
+use Lingua::EN::Sentence qw(get_sentences);
+use Unicode::Normalize 'normalize';
+
+binmode(STDOUT, ":utf8");
+
+$filename = "rowData.txt";
+open FILE, "<:encoding(utf-8)", $filename or die "can't find file ".$filename;
+while ($readinline = <FILE>) {
+    if ($readinline =~ /^\n/) {
+        next;
+    }
+    if ($readinline =~ /^\d/) {
+        while ($readinline = <FILE>) {
+            if ($readinline =~ /^\n/) {
+                last;
+            }
+        }
+    }
+
+    #title
+    $readinline = <FILE>;
+    $readinline =~ s/\R+//g;
+    $title = "Title - " . $readinline;
+    while ($readinline = <FILE>) {
+        if ($readinline =~ /^\n/) {
+            last;
+        }
+        $readinline =~ s/\R+//g;
+        $title .= $readinline;
+    }
+
+    #author
+    while ($readinline = <FILE>) {
+        if ($readinline =~ /^\n/) {
+            last;
+        }
+    }
+
+    $abstract = "";
+    $readinline = <FILE>;
+    if ($readinline =~ /^Author/) {
+        while ($readinline = <FILE>) {
+            if ($readinline =~ /^\n/) {
+                last;
+            }
+        }
+    } else {
+        #here comes the abstract
+        $abstract .= $readinline;
+    }
+
+    while ($readinline = <FILE>) {
+        if ($readinline=~ /^\n/) {
+            last;
+        }
+        $readinline =~ s/\R//g; #remove last newline
+        $abstract .= $readinline;
+    }
+
+    #PMID
+    while ($readinline = <FILE>) {
+        if ($readinline =~ /PMID: (\d+)/) {
+            $pmid = $1;
+            last;
+        }
+    }
+    if ($title =~ /Title - \[/) {
+        next;
+    }
+    print "PMID - $pmid\n";
+    # my $sref = get_sentences($abstract);
+    # foreach my $sentence (@$sref) {
+    #     #$sentence = normalize $sentence
+    #     if ($sentence =~ /heart rate[\w,\s]+age/gi) {
+    #         print $sentence . "\n";
+    #     }
+    # }
+    # print "\n\n";
+    print $title . "\n\n" . $abstract . "\n\n\n"; 
+}

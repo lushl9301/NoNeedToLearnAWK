f18953501583b84bb30206a9455a05dbe66723b6
Lu Shengliang <lushl9301@gmail.com>
2014-12-08 21:02:42 +0800

    first commit; split sentence

---
+++ ureca/fraily_stroke_IMU_EMG/select-sentence.pl
@@ -0,0 +1,37 @@
+use warnings;
+use Lingua::EN::Sentence qw(get_sentences);
+
+binmode(STDOUT, ":utf8");
+
+$filename = "myFormat.txt";
+open FILE, "<:encoding(utf-8)", $filename or die "can't find file ".$filename;
+
+while ($readinline = <FILE>) {
+    if ($readinline =~ /^\n/) {
+        next;
+    }
+    $PMID = $readinline;
+    $readinline = <FILE>; #title here
+    $readinline = <FILE>; #empty line
+    $abstract = <FILE>; #abstract here
+
+    chomp($abstract);
+    $counter = 0;
+    my $sref = get_sentences($abstract);
+    $useful = "";
+    foreach my $sentence (@$sref) {
+        $useful .= $sentence . "\n";
+        $counter += 1;
+        # #$sentence = normalize $sentence
+        # if ($sentence =~ /frailty/i) {   #contains heart rate
+        #     #if ($sentence =~ /(\s|^)(age|aging|aged|ages)(\s|\W)/i) {
+        #         #contains age/ageing/aged/ages
+        #         $counter += 1;
+        #         $useful .= $sentence . "\n";
+        #     #}
+        # }
+    }
+    if ($counter > 0) {
+        print "$PMID" . $useful . "\n";
+    }
+}
